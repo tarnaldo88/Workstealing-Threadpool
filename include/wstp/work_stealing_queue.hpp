7dcdcfefdeb0fd3fd313d01f3cfe7e272ccd8032
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-26 13:29:48 -0800

    implemented header for work stealing queue

--- include/wstp/work_stealing_queue.hpp
+++ include/wstp/work_stealing_queue.hpp
@@ -6,5 +6,20 @@
 #include "task.hpp"
 
 namespace wstp {
+    class WorkStealingQueue {
+        public:
+            WorkStealingQueue() = default;
+            ~WorkStealingQueue() = default;
 
+            WorkStealingQueue(const WorkStealingQueue&) = delete;
+            WorkStealingQueue& operator=(const WorkStealingQueue&) = delete;
+
+            void push_bottom(Task&& task);
+            std::optional<Task> pop_bottom();
+            std::optional<Task> steal_top();
+
+        private:
+            std::deque<Task> deque_;
+            std::mutex mutex_;
+    };
 }
