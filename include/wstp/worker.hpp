d416e5502b980b66e50dffe2eb976a2622f9a41b
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-26 14:28:37 -0800

    Completed header for worker class

--- include/wstp/worker.hpp
+++ include/wstp/worker.hpp
@@ -7,10 +7,30 @@
 #include "work_stealing_queue.hpp"
 
 namespace wstp {
+
+    class ThreadPool;
+
     class Worker {
-        public:
+    public:
         Worker(size_t id, ThreadPool* pool, WorkStealingQueue* localQueue);
+        ~Worker();
+
+        Worker(const Worker&) = delete;
+        Worker& operator=(const Worker&) = delete;
+
+        Worker(Worker&&) = delete;
+        Worker& operator=(Worker&&) = delete;
+
         void notify_stop();
         void join();
+
+    private:
+        void run();
+
+        size_t id_;
+        ThreadPool* pool_;
+        WorkStealingQueue* localQueue_;
+        std::thread thread_;
+        std::atomic<bool> stop_{false};
     };
  }
