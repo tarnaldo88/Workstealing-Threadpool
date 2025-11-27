88bcc468402e33c249e8ee1a14f5fcfdbf8b0b3d
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-27 13:09:11 -0800

    starting to implement FutureState class.

--- include/wstp/futures.hpp
+++ include/wstp/futures.hpp
@@ -7,5 +7,23 @@
 #include <optional>
 
 namespace wstp {
+    template<typename T>
+    class FutureState {
+    public:
+        void set_value(T val) {
+            std::lock_guard<std::mutex> lock(mtx_);
+            if (ready_) throw std::runtime_error("Value already set");
+            value_ = std::move(val);
+            ready_ = true;
+        
+        cv_.notify_all();
+        }
     
+    private:
+        mutable std::mutex mtx_;
+        std::condition_variable cv_;
+        std::optional<T> value_;
+        std::exception_ptr exception_;
+        bool ready_ = false;
+    };
 }
