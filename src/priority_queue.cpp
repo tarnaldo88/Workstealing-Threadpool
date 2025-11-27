cc852b03cd33aed5c6817d443082d94eab21ff05
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-27 11:50:51 -0800

    implemented push

--- src/priority_queue.cpp
+++ src/priority_queue.cpp
@@ -2,6 +2,9 @@
 
 void PrioritizedTaskQueue::push(wstp::Task task, TaskPriority priority)
 {
+    std::lock_guard<std::mutex> lock(mtx_);
+    queue_.push(PrioritizedTask{task, priority});
+    cv_.notify_one();
 }
 
 wstp::Task PrioritizedTaskQueue::pop()
